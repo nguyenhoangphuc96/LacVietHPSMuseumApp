fd1aa9e24d51c6352344d9ed05bf1f92520668d1
nguyenhoangphuc96 <nguyenhoangphuc171196@gmail.com>
2018-05-14 09:28:26 +0700

    init project

---
+++ app/src/main/java/vn/com/lacviet/lacviethpsmuseummanagementapp/MainScreen/Fragment3.java
@@ -0,0 +1,31 @@
+package vn.com.lacviet.lacviethpsmuseummanagementapp.MainScreen;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import vn.com.lacviet.lacviethpsmuseummanagementapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Fragment3 extends Fragment {
+
+
+    public Fragment3() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view= inflater.inflate(R.layout.fragment3, container, false);
+        return view;
+    }
+
+}
