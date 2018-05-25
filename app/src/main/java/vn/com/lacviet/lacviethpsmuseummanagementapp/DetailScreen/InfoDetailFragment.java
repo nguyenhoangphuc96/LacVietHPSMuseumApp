5a281f5b61458edec7f5ba31a346b33eb60b79a1
nguyenhoangphuc96 <nguyenhoangphuc171196@gmail.com>
2018-05-25 09:14:42 +0700

    complete show 3d model(70%)

--- app/src/main/java/vn/com/lacviet/lacviethpsmuseummanagementapp/DetailScreen/InfoDetailFragment.java
+++ app/src/main/java/vn/com/lacviet/lacviethpsmuseummanagementapp/DetailScreen/InfoDetailFragment.java
@@ -1,5 +1,6 @@
 package vn.com.lacviet.lacviethpsmuseummanagementapp.DetailScreen;
 
+import android.content.Intent;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 
@@ -10,8 +11,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import vn.com.lacviet.lacviethpsmuseummanagementapp.R;
+import vn.com.lacviet.lacviethpsmuseummanagementapp.Show3DModelScreen.Main;
 import vn.com.lacviet.lacviethpsmuseummanagementapp.adapter.InfoDetailPagerAdapter;
 
 public class InfoDetailFragment extends Fragment {
@@ -19,6 +22,7 @@ public class InfoDetailFragment extends Fragment {
 
     private ViewPager pager;
     private TabLayout tabLayout;
+    private TextView tvSameExhibit;
 
     @Nullable
     @Override
@@ -26,10 +30,21 @@ public class InfoDetailFragment extends Fragment {
         View view= inflater.inflate(R.layout.fragment_info_detail, container, false);
         addControls(view);
         showDataToView();
+        addEvent();
 
 
         return view;
 
+    }
+
+    private void addEvent() {
+        tvSameExhibit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), Main.class);
+                getContext().startActivity(intent);
+            }
+        });
     }
 
     private void showDataToView() {
@@ -44,6 +59,7 @@ public class InfoDetailFragment extends Fragment {
     private void addControls(View view) {
         pager = view.findViewById(R.id.container);
         tabLayout = view.findViewById(R.id.tabLayoutDetail);
+        tvSameExhibit = view.findViewById(R.id.tvSameExhibit);
     }
 
 
